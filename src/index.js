830777f65597bc73dacbef0581ae9073c46309f4
JasonSooter <jason.sooter@gmail.com>
2018-08-30 22:17:41 -0500

    Add links to the 3 routes temporarily for easier testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import App from './components/App';
 import Login from './components/AuthPages/Login';
@@ -11,6 +11,19 @@ import './index.css';
 ReactDOM.render(
   <Router>
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+
+      <hr />
       <Route exact path="/" component={App} />
       <Route path="/login" component={Login} />
       <Route path="/signup" component={Signup} />
