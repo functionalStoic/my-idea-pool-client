49e732016366cee22cf77073a787dcf8208586c9
JasonSooter <jason.sooter@gmail.com>
2018-09-05 21:53:55 -0500

    Removed redux-api-middleware until I am sure I want to use it

--- src/store.js
+++ src/store.js
@@ -1,21 +1,16 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { apiMiddleware } from 'redux-api-middleware';
 import logger from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
 import rootReducer from './reducers';
 
-const createStoreWithMiddleware = composeWithDevTools(
-  applyMiddleware(apiMiddleware, thunkMiddleware, logger)
-)(createStore);
-
-export default createStoreWithMiddleware(
+export default createStore(
   rootReducer,
-  window.REDUX_INITIAL_STATE
-  // composeWithDevTools(
-  //   applyMiddleware(
-  //     thunkMiddleware, // lets us dispatch() functions
-  //     logger // neat middleware that logs actions
-  //   )
-  // )
+  {},
+  composeWithDevTools(
+    applyMiddleware(
+      thunkMiddleware, // lets us dispatch() functions
+      logger // neat middleware that logs actions
+    )
+  )
 );
