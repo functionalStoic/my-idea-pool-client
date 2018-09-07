7f9741f92782a01b3c2e0a3a3d77a119093315f3
JasonSooter <jason.sooter@gmail.com>
2018-09-07 17:51:33 -0500

    Create ideas api ajax calls - createIdea - deleteIdea - getIdeas - updateIdea

---
+++ src/api/ideas.api.js
@@ -0,0 +1,45 @@
+import axiosInstance from './axiosInstance';
+
+export default class AuthApi {
+  static async createIdea(content, impact, ease, confidence) {
+    try {
+      const { data } = await axiosInstance.post('/ideas', {
+        content,
+        impact,
+        ease,
+        confidence
+      });
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async deleteIdea(id) {
+    try {
+      return axiosInstance.delete(`/ideas/${id}`);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async getIdeas(page) {
+    try {
+      const { data } = await axiosInstance.get(`/ideas`, { page });
+
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async updateIdea(id, content, impact, ease, confidence) {
+    try {
+      const { data } = await axiosInstance.put(`/ideas/${id}`);
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
