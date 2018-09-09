8ec4d2fb8cb291ace5e3873002d72c2404b30073
JasonSooter <jason.sooter@gmail.com>
2018-09-08 23:49:49 -0500

    Add call to get page of ideas on mounting of App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-
+import { getIdeas } from '../../actions';
 import AppWrapper from '../shared/AppWrapper';
 
 import Header from './Header';
 import Ideas from './Ideas';
 
-const App = props =>
-  !props.isAuthenticated ? (
-    <Redirect to="/login" />
-  ) : (
-    <AppWrapper {...props}>
-      <Wrapper>
-        <Header />
-        <Ideas {...props} />
-      </Wrapper>
-    </AppWrapper>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(getIdeas());
+  }
+  render() {
+    return !this.props.isAuthenticated ? (
+      <Redirect to="/login" />
+    ) : (
+      <AppWrapper {...this.props}>
+        <Wrapper>
+          <Header {...this.props} />
+          <Ideas {...this.props} />
+        </Wrapper>
+      </AppWrapper>
+    );
+  }
+}
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -29,6 +35,7 @@ const Wrapper = styled.div`
 const mapStateToProps = ({ auth, ideas }) => {
   return {
     isAuthenticated: auth.isAuthenticated,
+    isFetching: ideas.isFetching,
     ideas: ideas.ideas
   };
 };
