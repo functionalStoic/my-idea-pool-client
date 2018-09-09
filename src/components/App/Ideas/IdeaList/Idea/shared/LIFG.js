7ebac8eb4deb2e4f61edf2edb14caaa177c933a4
JasonSooter <jason.sooter@gmail.com>
2018-09-08 23:56:02 -0500

    Created reusable Li component to set flex-grow attribute

---
+++ src/components/App/Ideas/IdeaList/Idea/shared/LIFG.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export default styled.li`
+  flex-grow: ${props => props.num} !important;
+`;
--- src/components/App/Ideas/IdeaList/index.js
+++ src/components/App/Ideas/IdeaList/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Idea from './Idea';
-
+import LIFG from './Idea/shared/LIFG';
 export default props => {
   return (
     props.ideas.length > 0 && (
       <div>
         <UlWrapper>
-          <li style={{ flexGrow: 7.3 }} />
-          <li style={{ flexGrow: 1 }}>Impact</li>
-          <li style={{ flexGrow: 1 }}>Ease</li>
-          <li style={{ flexGrow: 1 }}>Confidence</li>
-          <li style={{ flexGrow: 1 }}>
+          <LIFG num={7.3} />
+          <LIFG num={1}>Impact</LIFG>
+          <LIFG num={1}>Ease</LIFG>
+          <LIFG num={1}>Confidence</LIFG>
+          <LIFG num={1}>
             <strong>Avg.</strong>
-          </li>
-          <li style={{ flexGrow: 2 }} />
+          </LIFG>
+          <LIFG num={2} />
         </UlWrapper>
-
         {props.ideas.map(idea => (
           <Idea key={idea.id} idea={idea} {...props} />
         ))}
